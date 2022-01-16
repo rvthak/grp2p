c2d2edcad383ce190fdfa4adf97ac7dffead6a7b
Giannis Rovithakis <giannis.rovithakis@gmail.com>
2022-01-16 18:08:02 +0200

    Added external mandos scripts: init and deploy steps

--- src/grp2p.rs
+++ src/grp2p.rs
@@ -2,8 +2,6 @@
 
 elrond_wasm::imports!();
 
-/// One of the simplest smart contracts possible,
-/// it holds a single variable in storage, which anyone can increment.
 #[elrond_wasm::derive::contract]
 pub trait Grp2p {
 
@@ -19,15 +17,61 @@ pub trait Grp2p {
         self.id().set(&counter);
     }
 
+
+    // ------------------------- Owner ------------------------- 
+
+    // // Add a new token on the token whitelist
+    // #[endpoint]
+    // fn add_token(
+    //     &self,
+    //     token: TokenIdentifier,
+    // ) -> SCResult<()> {
+
+    //     // Ensure caller is owner
+    //     self.require_permissions()?;
+
+    //     // Add the token to the token whitelist
+    //     self.whitelist().push(&token);
+
+    //     Ok(())
+    // }
+
+    // // Remove a token from the token whitelist
+    // #[endpoint]
+    // fn remove_token(
+    //     &self,
+    //     token: TokenIdentifier,
+    // ) -> SCResult<()> {
+
+    //     // Ensure caller is owner
+    //     self.require_permissions()?;
+
+    //     // Remove the token from the whitelist
+    //     self.whitelist().pop(&token);
+
+    //     // IMPORTANT: Don't clear any existing swap orders pending
+    //     // for this token. We just removed the user's ability to
+    //     // create new orders but we can't cancel the existing ones
+
+    //     Ok(())
+    // }
+
+
     // -------------------------- Utils -------------------------- 
 
     fn add(&self, value : &u32){ self.id().update(|id| (*id) += *value ); }
 
     // ------------------------- Storage ------------------------- 
 
+    // The swap order-id counter
     #[storage_mapper("id")]
     fn id(&self) -> SingleValueMapper<BigUint>;
 
+    // A supported esdt token whitelist
+    #[storage_mapper("whitelist")]
+    fn whitelist(&self) -> SetMapper<TokenIdentifier>;
+
+    // Store and retrieve the contract's owner
     #[storage_set("owner")]
     fn set_owner(&self, address: &ManagedAddress);
 
